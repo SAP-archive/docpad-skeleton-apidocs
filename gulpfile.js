9d257e9368149fe423e4d53e67edf2a017098ff6
Domagala, Sebastian <sebastian.domagala@sap.com>
2016-09-22 15:30:08 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 
 const gulp = require('gulp'),
   chewie = require('chewie'),
-  validation = require('./gulp_tasks/deployment/validationMetadata.js'),
   argv = require('yargs')
   .alias('s', 'section')
   .alias('t', 'topics')
