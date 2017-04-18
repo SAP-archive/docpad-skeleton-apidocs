58b1b8057036de1364073483db885f085d5d2622
Hadyk, Alan <alan.hadyk@sap.com>
2017-04-18 15:00:11 +0200

    Changed docpad-skeleton-apidocs-sampledata to chewie-sample-data

--- chewieConfig.js
+++ chewieConfig.js
@@ -10,7 +10,7 @@ const config = {
 
   registry: {
     location: process.env.REGISTRY_LOCATION || 'remote',
-    path: process.env.REGISTRY_PATH || 'https://github.com/YaaS/docpad-skeleton-apidocs-sampledata.git',
+    path: process.env.REGISTRY_PATH || 'https://github.com/YaaS/chewie-sample-data.git',
     fileName: 'docu_registry.json',
     branch: process.env.docuBranch || 'master',
     shortVersionFileName: 'shrinkedRegistry.json',
